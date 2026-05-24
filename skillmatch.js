956b3c1aa97eb90849e78e42fa544d2fedc980de
Kauan-01 <k.kantovick2008@gmail.com>
2026-05-24 19:10:47 -0300

    lint: correção ortográfica comentarios

--- skillmatch.js
+++ skillmatch.js
@@ -40,7 +40,7 @@ const vagas = [
     salario: 4200 }
 ];
 
-//FUNÇÃO PARA CALCULAR PERCENTUAL DE CMPATIBILIDADE DAS HABIBILIDADES DO CANDIDATO COM REUUISITOS DA VAGA
+//FUNÇÃO PARA CALCULAR PERCENTUAL DE COMPATIBILIDADE DAS HABILIDADES DO CANDIDATO COM REQUISITOS DA VAGA
 function calcularCompatibilidade(candidato, vaga) {
   let match = 0;
   
@@ -56,7 +56,7 @@ function calcularCompatibilidade(candidato, vaga) {
          
 }
 
-//FUNÇAO PARA LISTAR AS HABILIDADE OOMPATIVEIS DO CANDIDATO
+//FUNÇÃO PARA LISTAR AS HABILIDADE COMPATIVEIS DO CANDIDATO
 function habilidadesEncontradas(candidato, vaga) {
   return vaga.requisitos.filter((requisito) => {
     return candidato.habilidades.includes(requisito);
@@ -74,7 +74,7 @@ function classificaCompatibilidade(percentual) {
   }
 }
 
-//FUNÇAO PARA LISTAR AS HABILIDADE FALTANTES DO CANDIDATO
+//FUNÇÃO PARA LISTAR AS HABILIDADES FALTANTES DO CANDIDATO
 function listaHabilidadesFaltantes(candidato, vaga) {
   return vaga.requisitos.filter((requisito) => {
     return !candidato.habilidades.includes(requisito);
